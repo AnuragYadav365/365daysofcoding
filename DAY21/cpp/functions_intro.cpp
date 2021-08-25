4600d8fca4bb008e4408e2a3a0e3b045274612b2
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-08-25 23:47:51 +0530

    Added cpp functions introduction program

---
+++ DAY21/cpp/functions_intro.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int greatest = a;
+    for(int i = 0; i<4; i++){
+        if(greatest < b){
+        greatest = b;
+        }
+        else if(greatest < c){
+            greatest = c;
+        }
+        else if(greatest < d){
+            greatest = d;
+        }
+    }
+    
+    return greatest;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
