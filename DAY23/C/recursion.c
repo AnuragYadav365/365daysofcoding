76e456949175e42c5362cadd8c1bf1fe9f2aa37b
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-08-27 20:12:11 +0530

    Added C resursion program for calculating AP Sum

---
+++ DAY23/C/recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  int nterm;
+  int d = b - a;
+  nterm = a + ((n-1)*d);
+  return nterm;
+}
+
+int main() {
+    int n, a, b, c;
+
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+
+    printf("%d", ans);
+    return 0;
+}
