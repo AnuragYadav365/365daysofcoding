387b14258662d717958f2a0cc604c2387e0e1955
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-09-08 10:17:38 +0530

    added codeforces 546A solution
    Soldier and bananas

---
+++ DAY35/soldier_bananas.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int k,n,w;
+    cin >> k >> n >> w;
+    int price[4];
+    int count = 0;
+    //to calculate price upto w(total number of bananas needed)
+    for(int i=0; i<w; i++){
+        price[i] = (i+1) * k;
+        count = count + price[i];
+    }
+
+    //if soldier buy more expensive then the money he has
+    if(count > n){
+        cout << abs(count-n);
+    }
+
+    //if the price is in the budget of soldier only, no need to borrow
+    else{
+        cout << "0";
+    }
+    return 0;
+}
