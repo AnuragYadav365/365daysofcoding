997fa815ed98c235946ed79323ff735fec80647c
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-09-08 17:33:06 +0530

    Added Big Brother 791A solution

---
+++ DAY35/Bear_Bigbrother.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+
+    int year = 0;
+    //loop till limak's wight is less than bob's weight
+    while(a<=b){
+        a = 3*a; //weight is tripled after every year
+        b = 2*b; //Bob's weight is doubled after every year
+
+        year++;
+    }
+    cout << year;
+    return 0;
+}
