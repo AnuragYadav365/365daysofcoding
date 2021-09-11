0c5dfc3a9e18505f047bd523c097231b91b0829e
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-09-11 23:22:30 +0530

    added george and accomodation solution

---
+++ DAY38/george_accomodation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; //number of rooms
+    cin >> n;
+
+    int p = 0;
+    int q = 0;
+    int roomcnt = 0; //storing valid rooms
+    //looping through every room
+    while(n--){
+        cin >> p >> q;
+
+        //room capacity - number of people must be grater than 2
+        //then it is a valid room
+        if(q - p >= 2){
+            roomcnt++;
+        }
+    }
+    cout << roomcnt;
+    return 0;
+}
