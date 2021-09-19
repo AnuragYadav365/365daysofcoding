b76514fc64a4c294191837cab67e590fb0fc0759
AnuragYadav365 <anuragyadavmnp@gmail.com>
2021-09-20 00:54:27 +0530

    Added divisible sum pair solution

---
+++ DAY46/Divisible_Sum_pair.cpp
@@ -0,0 +1,112 @@
+//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+//Anurag Yadav
+//https://github.com/AnuragYadav365
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+    int div_cnt = 0;
+    //loop which runs in pair
+    //first loop runs till n-1 index
+    for(int j=0; j<n-1; j++){
+        //second loop runs till n index
+        //creating a combination of distinct pairs
+        for(int p=j+1; p<n; p++){
+            if((ar[j] + ar[p])%k == 0 ){
+                // cout << "Pair divisible: " << ar[j] << ar[p] << endl;
+                div_cnt++;
+            }
+        }
+    }
+    return div_cnt;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split(rtrim(ar_temp_temp));
+
+    vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = divisibleSumPairs(n, k, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
